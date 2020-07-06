faeaebf312c51159c7b92267a7e3f270e3a507b9
yokn <discolouredattic@gmail.com>
2020-07-06 14:18:27 +0300

    add indexes

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,20 +4,30 @@
 def stock_picker(stock_array)
   highest_number = 0
   lowest_number = 100
-
+  highest_number_index = 0
+  lowest_number_index = 0
   # Iterate through the array to find the highest day and store it in a variable UNLESS it is the first day
   # Iterate through the array to find the lowest day and store it in a variable UNLESS it is the last day
-  stock_array.each do |number|
+  stock_array.each_with_index do |number, index|
     unless number == stock_array.first
-      highest_number = number if number > highest_number
+      if number > highest_number
+        highest_number = number
+        highest_number_index = index
+      end
     end
     next if number == stock_array.last
 
-    lowest_number = number if number < lowest_number
+    next unless number < lowest_number
+
+    unless index > highest_number_index
+      lowest_number = number
+      lowest_number_index = index
+    end
   end
-  puts highest_number
-  puts lowest_number
+  puts "buy on day #{lowest_number_index} for $#{lowest_number}"
+  puts "sell on day #{highest_number_index} for $#{highest_number}"
+  puts "for a profit of $#{highest_number - lowest_number}"
 end
 
 # Print the result
-stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+stock_picker([17, 15, 6, 9, 3, 8, 6, 1, 10])
