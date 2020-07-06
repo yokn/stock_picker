c4af3291625e08b55448e2d26a9b19fa93eb9226
yokn <discolouredattic@gmail.com>
2020-07-06 12:00:27 +0300

    initial commit

---
+++ stock_picker.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Get the array with stock prices in it from the user, and store it
+def stock_picker(stock_array)
+  new_array = stock_array.sort
+  puts new_array
+end
+# Iterate through the array to find the lowest day and store it in a variable UNLESS it is the last day
+# Iterate through the array to find the highest day and store it in a variable UNLESS it is the first day
+# Print the result
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
