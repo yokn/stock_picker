a4fdccb2ce8e1bfe9293608ab521a479cc6c2a9c
yokn <discolouredattic@gmail.com>
2020-07-06 14:10:17 +0300

    convert to #each

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,12 +2,22 @@
 
 # Get the array with stock prices in it from the user, and store it
 def stock_picker(stock_array)
-  new_array = stock_array.sort
-  new_array.shift if new_array.first == stock_array.last
-  new_array.pop if new_array.last == stock_array.first
-  puts new_array
+  highest_number = 0
+  lowest_number = 100
+
+  # Iterate through the array to find the highest day and store it in a variable UNLESS it is the first day
+  # Iterate through the array to find the lowest day and store it in a variable UNLESS it is the last day
+  stock_array.each do |number|
+    unless number == stock_array.first
+      highest_number = number if number > highest_number
+    end
+    next if number == stock_array.last
+
+    lowest_number = number if number < lowest_number
+  end
+  puts highest_number
+  puts lowest_number
 end
-# Iterate through the array to find the lowest day and store it in a variable UNLESS it is the last day
-# Iterate through the array to find the highest day and store it in a variable UNLESS it is the first day
+
 # Print the result
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
